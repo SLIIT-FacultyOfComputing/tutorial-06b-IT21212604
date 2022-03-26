a2118a626cf1e95891e6b9540c2ef4e9b564ad37
IT21212604 <99326713+IT21212604@users.noreply.github.com>
2022-03-26 05:11:34 +0000

    completed tutorial 06

--- Box.h
+++ Box.h
@@ -4,6 +4,13 @@ class Box {
        int width;
        int height;
     public:
+       void setLength(double l);
+       void setWidth(double w);
+       void setHeight(double h);
+
+       int getLength();
+       int getWidth();
+       int getHeight();
        // write prototypes of setters for length, width and height
        // write prototypes of getters for length, width and height
  
--- Exercise02.cpp
+++ Exercise02.cpp
@@ -8,6 +8,7 @@ int main() {
   
   
    // ======= DO NOT CHANGE THE INPUT ===========
+  Box box1;
    int height, length, width;
    cout << "Enter the Height of the Box : ";
    cin >> height;
@@ -20,11 +21,16 @@ int main() {
    // 2. Use setters assign height, length, width throw
   
    // === DO NOT CHANGE THE OUTPUT =============
-   cout << "Box Height " << box1.getHeight() << endl;
-   cout << "Box Length " << box1.getLength() << endl;
-   cout << "Box Width " << box1.getWidth() << endl;
-   cout << "Volume of Box is " << box1.calcVolume() << endl;
+   //cout << "Box Height " << box1.getHeight() << endl;
+  // cout << "Box Length " << box1.getLength() << endl;
+   //cout << "Box Width " << box1.getWidth() << endl;
+   //cout << "Volume of Box is " << box1.calcVolume() << endl;
    // ==========================================
+  box1.setLength(length);
+  box1.setWidth(width);
+  box1.setHeight(height);
+
+  
       return 0;
 }
 
